de38f8f115fe22c7a845a8b7d6b8aca555f6f701
Ryan Jackson <rcjackson96@gmail.com>
2022-11-22 18:27:25 -0600

    Schema and seeds added

---
+++ db/Schema.sql
@@ -0,0 +1,19 @@
+--This is the Schema
+--Drop DATABASE
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id 
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id 
+);
+
+CREATE TABLE employee(
+
+);
---
+++ db/Seeds.sql
@@ -0,0 +1,10 @@
+--This is the Seeds.sql
+--Department
+INSERT INTO department (name)
+VALUES ("");
+
+INSERT INTO department (name)
+VALUES ("");
+
+INSERT INTO department (name)
+VALUES ("");
