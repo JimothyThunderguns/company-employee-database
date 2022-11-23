974dcd345dcf433c3234b51d35aeea75e4092b65
Ryan Jackson <rcjackson96@gmail.com>
2022-11-22 18:43:33 -0600

    improved javascript

--- index.js
+++ index.js
@@ -1,9 +1,87 @@
+//imports and dependencies
 import mysql from 'mysql2'
 import inquirer from 'inquirer'
 
+//add the connection
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root'
-    database:
+    password: 'root',
+    database: 'company-employee-database.db'
 })
+//==================================================================================================================================
+//VIEW
+//==================================================================================================================================
+//view all of the departments 
+const viewDepartments = async () => {
+
+}
+
+//view employees
+const viewEmployee = async () => {
+
+}
+
+//view roles
+const viewRoles = async () => {
+
+}
+
+
+//view Salary
+const viewSalary = async () => {
+
+}
+
+//view employees
+const viewEmployeeDepartment = async () => {
+
+}
+
+//view employees
+const viewEmployeeManager = async () => {
+
+}
+
+//==================================================================================================================================
+//DEPARTMENTS
+//==================================================================================================================================
+
+//add department
+const addDepartments = async () => {
+    const answer = await inquirer.prompt([
+
+    ])
+}
+
+//==================================================================================================================================
+//ROLES
+//==================================================================================================================================\
+//add department
+const addRole = async () => {
+    const answer = await inquirer.prompt([
+
+    ])
+}
+
+//==================================================================================================================================
+//EMPLOYEES
+//==================================================================================================================================
+//add department
+const addEmployee = async () => {
+    const answer = await inquirer.prompt([
+
+    ])
+}
+
+const deleteEmployee = async() => {
+
+}
+
+const updateEmployeeRole = async() => {
+
+}
+
+const updateEmployeeManager = async() => {
+
+}
