ee9a3c013fc793a5428bdca04cb879514d3e7b0d
Ryan Jackson <rcjackson96@gmail.com>
2022-11-24 10:31:13 -0600

    rework for the javascript

--- index.js
+++ index.js
@@ -1,46 +1,76 @@
 //imports and dependencies
-import mysql from 'mysql2'
-import inquirer from 'inquirer'
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+require('console.table');
+
+//prompts
+
+const messagePrompt = {
+    viewEmployees: "View all Employees",
+    viewEmployeeDepartment: "View all employees by their Department",
+    viewEmployeeManager: "View all employees by their Manager",
+    viewRoles: "View all Employee Roles",
+    addEmployee: "Add an Employee",
+    deleteEmployeee: "Remove an Employee",
+    updateRole: "Update an Employee Role",
+    updateManager: "Update the Manager for an Employee",
+    exit: "Exit Program"
+};
 
 //add the connection
 const connection = mysql.createConnection({
     host: 'localhost',
+//Port
+    port:3306,
+    //username
     user: 'root',
+
+    //password
     password: 'root',
-    database: 'company-employee-database.db'
-})
+
+    //database
+    database: 'employee_db'
+});
+
+
 //==================================================================================================================================
 //VIEW
 //==================================================================================================================================
 //view all of the departments 
 const viewDepartments = async () => {
+    try {
+        const [results] = await connection.promise()
+    }
 
 }
 
 //view employees
 const viewEmployee = async () => {
-
+    const [results] = await connection.promise()
 }
 
 //view roles
 const viewRoles = async () => {
+    const [results] = await connection.promise()
 
 }
 
 
 //view Salary
 const viewSalary = async () => {
-
+    try {
+        const[results] = await connection.promise.query 
+    }
 }
 
 //view employees
 const viewEmployeeDepartment = async () => {
-
+    const [results] = await connection.promise()
 }
 
 //view employees
 const viewEmployeeManager = async () => {
-
+    const [results] = await connection.promise()
 }
 
 //==================================================================================================================================
